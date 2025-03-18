8cded1f3b23bce3bc43f055a25278dad389559ce
rabiyasalehjee <rabiya.salehjee@gmail.com>
2025-03-18 13:41:04 +0800

    adjusted animation duration

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,50 +2,49 @@ import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import './main.css';
 
-import img1 from '../assets/img1.jpg'; 
-import img2 from '../assets/img2.jpg'; 
-import img3 from '../assets/img3.jpg'; 
-import img4 from '../assets/img4.jpg'; 
-import img5 from '../assets/img5.jpg';  
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+import img4 from '../assets/img4.jpg';
+import img5 from '../assets/img5.jpg';
 
-const data = [   
-  {     
-    place: 'Yellowstone National Park, Wyoming',     
-    title: 'BLACK SAND',     
-    title2: 'BASIN',     
-    description: 'Black Sand Basin in Yellowstone National Park, Wyoming, is a stunning geothermal area known for its vibrant hot springs, geysers, and steaming pools. The contrast of dark volcanic sand with the colorful thermal waters creates a mesmerizing landscape.',     
-    image: img1,   
-  },   
-  {     
-    place: 'Yosemite National Park, USA',     
-    title: 'SENTINEL',     
-    title2: 'DOME',     
-    description: 'Sentinel Dome in Yosemite National Park offers breathtaking panoramic views of the surrounding valley, including El Capitan and Half Dome. A relatively short hike rewards visitors with one of the most iconic vistas in the park.',     
-    image: img2,   
-  },   
-  {     
-    place: 'Bryce Canyon National Park, USA',     
-    title: 'STONE',     
-    title2: 'GATE',     
-    description: 'The Stone Gate in Bryce Canyon National Park stands as a natural rock formation, framing the breathtaking hoodoos and red rock amphitheaters that make this park a geological wonder.',     
-    image: img3,   
-  },   
-  {     
-    place: 'Durdle Door, Dorset, England',     
-    title: 'DURDLE',     
-    title2: 'DOOR',     
-    description: 'Durdle Door, a magnificent limestone arch on the Jurassic Coast of Dorset, England, is one of the most photographed and iconic coastal landmarks in the UK. Its scenic beauty and clear blue waters make it a favorite destination for visitors.',     
-    image: img4,   
-  },   
-  {     
-    place: 'Piha Beach, New Zealand',     
-    title: 'LION',     
-    title2: 'ROCK',     
-    description: 'Piha Beach, home to the striking Lion Rock, is a rugged black sand beach on New Zealand’s west coast. Popular among surfers and nature lovers, it offers dramatic coastal scenery and a deep cultural significance to the Māori people.',     
-    image: img5,   
-  }, 
+const data = [
+  {
+    place: 'Yellowstone National Park, Wyoming',
+    title: 'BLACK SAND',
+    title2: 'BASIN',
+    description: 'Black Sand Basin in Yellowstone National Park, Wyoming, is a stunning geothermal area known for its vibrant hot springs, geysers, and steaming pools. The contrast of dark volcanic sand with the colorful thermal waters creates a mesmerizing landscape.',
+    image: img1,
+  },
+  {
+    place: 'Yosemite National Park, USA',
+    title: 'SENTINEL',
+    title2: 'DOME',
+    description: 'Sentinel Dome in Yosemite National Park offers breathtaking panoramic views of the surrounding valley, including El Capitan and Half Dome. A relatively short hike rewards visitors with one of the most iconic vistas in the park.',
+    image: img2,
+  },
+  {
+    place: 'Bryce Canyon National Park, USA',
+    title: 'STONE',
+    title2: 'GATE',
+    description: 'The Stone Gate in Bryce Canyon National Park stands as a natural rock formation, framing the breathtaking hoodoos and red rock amphitheaters that make this park a geological wonder.',
+    image: img3,
+  },
+  {
+    place: 'Durdle Door, Dorset, England',
+    title: 'DURDLE',
+    title2: 'DOOR',
+    description: 'Durdle Door, a magnificent limestone arch on the Jurassic Coast of Dorset, England, is one of the most photographed and iconic coastal landmarks in the UK. Its scenic beauty and clear blue waters make it a favorite destination for visitors.',
+    image: img4,
+  },
+  {
+    place: 'Piha Beach, New Zealand',
+    title: 'LION',
+    title2: 'ROCK',
+    description: 'Piha Beach, home to the striking Lion Rock, is a rugged black sand beach on New Zealand’s west coast. Popular among surfers and nature lovers, it offers dramatic coastal scenery and a deep cultural significance to the Māori people.',
+    image: img5,
+  },
 ];
-
 
 const Main = () => {
   useEffect(() => {
@@ -90,7 +89,6 @@ const Main = () => {
         opacity: 0,
         zIndex: 60,
       });
-      gsap.set("nav", { y: -200, opacity: 0 });
 
       gsap.set(getCard(active), {
         x: 0,
@@ -159,7 +157,6 @@ const Main = () => {
         });
       });
       gsap.to("#pagination", { y: 0, opacity: 1, ease, delay: startDelay });
-      gsap.to("nav", { y: 0, opacity: 1, ease, delay: startDelay });
       gsap.to(detailsActive, { opacity: 1, x: 0, ease, delay: startDelay });
     };
 
@@ -273,35 +270,6 @@ const Main = () => {
   return (
     <>
       <div className="indicator"></div>
-      <nav>
-        <div>
-          <div className="svg-container">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-            </svg>
-          </div>
-          <div>Globe Express</div>
-        </div>
-        <div>
-          <div className="active">Home</div>
-          <div>Holidays</div>
-          <div>Destinations</div>
-          <div>Flights</div>
-          <div>Offers</div>
-          <div>Contact</div>
-          <div className="svg-container">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-          </div>
-          <div className="svg-container">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-              <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-            </svg>
-          </div>
-        </div>
-      </nav>
-
       <div id="demo">
         {data.map((item, index) => (
           <React.Fragment key={index}>
@@ -318,12 +286,12 @@ const Main = () => {
 
       <div className="details" id="details-even">
         <div className="place-box">
-          <div className="text">Switzerland Alps</div>
+          <div className="text">Yellowstone National Park, Wyoming</div>
         </div>
-        <div className="title-box-1"><div className="title-1">SAINT</div></div>
-        <div className="title-box-2"><div className="title-2">ANTONIEN</div></div>
+        <div className="title-box-1"><div className="title-1">BLACK SAND</div></div>
+        <div className="title-box-2"><div className="title-2">BASIN</div></div>
         <div className="desc">
-          Tucked away in the Switzerland Alps, Saint Antönien offers an idyllic retreat for those seeking tranquility and adventure alike. It's a hidden gem for backcountry skiing in winter and boasts lush trails for hiking and mountain biking during the warmer months.
+          Black Sand Basin in Yellowstone National Park, Wyoming, is a stunning geothermal area known for its vibrant hot springs, geysers, and steaming pools. The contrast of dark volcanic sand with the colorful thermal waters creates a mesmerizing landscape.
         </div>
         <div className="cta">
           <button className="bookmark">
@@ -337,12 +305,12 @@ const Main = () => {
 
       <div className="details" id="details-odd">
         <div className="place-box">
-          <div className="text">Switzerland Alps</div>
+          <div className="text">Yellowstone National Park, Wyoming</div>
         </div>
-        <div className="title-box-1"><div className="title-1">SAINT</div></div>
-        <div className="title-box-2"><div className="title-2">ANTONIEN</div></div>
+        <div className="title-box-1"><div className="title-1">BLACK SAND</div></div>
+        <div className="title-box-2"><div className="title-2">BASIN</div></div>
         <div className="desc">
-          Tucked away in the Switzerland Alps, Saint Antönien offers an idyllic retreat for those seeking tranquility and adventure alike. It's a hidden gem for backcountry skiing in winter and boasts lush trails for hiking and mountain biking during the warmer months.
+          Black Sand Basin in Yellowstone National Park, Wyoming, is a stunning geothermal area known for its vibrant hot springs, geysers, and steaming pools. The contrast of dark volcanic sand with the colorful thermal waters creates a mesmerizing landscape.
         </div>
         <div className="cta">
           <button className="bookmark">
